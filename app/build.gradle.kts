9a235d0bb28860ee713596e20450a0ab43b63adf
unknownxsuperman-prog <unknownxsuperman@gmail.com>
2026-08-09 01:19:48 +0530

    Add Material Icons and Font Awesome dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
-
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -56,6 +55,8 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.animation:animation")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("br.com.devsrsouza.compose.icons:font-awesome:1.1.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
